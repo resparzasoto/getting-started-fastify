2b0a7d455416a42fa8b09306337eb43cc78d18b2
Rubén Esparza <rubenesparzasoto95@gmail.com>
2021-02-22 09:03:53 -0700

    first plugin

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ const fastify = require('fastify')({
   logger: true,
 });
 
-fastify.get('/', async (request, reply) => {
-  reply.send({ hello: 'world' });
-});
+fastify.register(require('./our-first-route'));
 
-const start = async () => {
-  try {
-    await fastify.listen(3000);
-  } catch (err) {
+fastify.listen(3000, function (err, address) {
+  if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-};
-
-start();
+  fastify.log.info(`server listening on ${address}`);
+});
